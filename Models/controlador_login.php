53e8092770c30cd9619236f2c08285afef903fd7
RacokLB <inversioneslowboy@gmail.com>
2025-10-15 11:06:33 -0400

    Update controlador_login.php

--- Models/controlador_login.php
+++ Models/controlador_login.php
@@ -1,5 +1,5 @@
 <?php
-//we call the connection we established with the database
+
 
     session_start();
 
@@ -23,31 +23,31 @@
         }
         
 
-//Here we indicate what we want to do when the login button that has name="signupbtn" is pressed.
+//Aqui indicamos que va a suceder cuando se valide que se presiono el boton signupboton.
     if(isset($_POST['signupbtn'])){
 
         require_once "../Config/abrir_conexion.php";
 
      
-        //Here we indicate what we are going to do when we enter the user and password data, we also indicate what will be reflected in case no data is entered within the fields
+       
         if (isset($_POST["user"]) && strlen($_POST['user']) >= 6 && strlen($_POST['user']) <= 8 && isset($_POST["password"])) {
-            //Here the data extracted from the global variable $_POST is stored in our local variables $user, $password 
+            
             $user = $_POST["user"];
            
             $password = $_POST["password"];
             
-            //the object this query is verificate user exits in the database
+           
             $query = $pdo->prepare("SELECT id, clave, rol FROM users WHERE user = :user");
             $query->bindParam(param:':user',var:$user);
             $query->execute();
             $user_data = $query->fetch(PDO::FETCH_ASSOC);
             
-            //if the user exits catch the clave from the database
+       
             if($user_data){
                 $hash = $user_data['clave'];
               
                 
-                //verification the password vs hash password in the database and if have match
+               
                 
                 if(password_verify(password: $password, hash: $hash)){
                    
@@ -58,12 +58,12 @@
                         $user_role_data = $query_role->fetch(mode:PDO::FETCH_ASSOC);
                         
                         
-                        // catch the user and rol and store into $_SESSION
+                      
                         if($user_role_data){
                             
                                 $rol = $user_role_data['rol'];
                                 $cedula = $user_role_data['user'];
-                                //insert into $_SESSION the data from the user for display in the index page
+                               
                                 $_SESSION['rol']= $rol;
                                 $_SESSION['user']= $cedula;
         
